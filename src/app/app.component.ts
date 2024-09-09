c1a6ab22d312f4cfb3a93814a4e202706f7b90d1
cs-emmanj <emmanj@cloudstaff.com>
2024-09-10 07:47:22 +0800

    feat: add initial design & data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,77 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'ecv';
+  public yearNow: number = new Date().getFullYear();
+
+  public email = 'emnljrz@gmail.com';
+  public location = 'General Santos City, Philippines';
+
+  private readonly technicalSkills = {
+    field: 'Technical Skills',
+    items: [
+      {
+        label: 'Core Skills',
+        values: [
+          'HTML, CSS, JavaScript',
+          'Angular',
+          '.Net Framework (C#, VB.Net)',
+          'Microsoft SQL Server',
+          'Git',
+        ],
+      },
+      {
+        label: 'Intermediate Skills',
+        values: ['Python', 'MySQL', 'PostgreSQL', 'MongoDB', 'SQLite', 'Dart'],
+      },
+      {
+        label: 'Exploring',
+        values: ['Rust', 'C++', 'Game Development'],
+      },
+    ],
+  };
+
+  private readonly softSkills = {
+    field: 'Soft Skills',
+    items: [
+      {
+        label: 'Team Player',
+        values: [],
+      },
+      {
+        label: 'Communication',
+        values: [],
+      },
+      {
+        label: 'Leadership and Mentoring',
+        values: [],
+      },
+    ],
+  };
+
+  private creativeSkill = {
+    field: 'Creative Skills',
+    items: [
+      { label: 'Photography', values: [] },
+      { label: 'Videography', values: [] },
+      {
+        label: 'Creative Tools',
+        values: [
+          'Adobe Photoshop',
+          'Adobe Illustrator',
+          'Adobe Lightroom',
+          'Final Cut',
+        ],
+      },
+    ],
+  };
+
+  public skills = [this.technicalSkills, this.softSkills, this.creativeSkill];
 }
